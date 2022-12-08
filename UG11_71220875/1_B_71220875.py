fd6a2f4161d9862b1905a854ba9a5588ee6a1828
Hanlig <117884462+Hanlig@users.noreply.github.com>
2022-12-08 13:01:18 +0700

    Add files via upload

---
+++ UG11_71220875/1_B_71220875.py
@@ -0,0 +1,4 @@
+c = int(input("masukan angka :"))
+for i in range(1, 5):
+    print("*"*i, ("* "*(c-i)))
+print (' ')
---
+++ UG11_71220875/2_B_71220875.py
@@ -0,0 +1,16 @@
+print(1 + 3)
+jumlah = 3
+banyak = 1
+hasil = jumlah + banyak
+print(hasil)
+print(2 + 2)
+jumlah = 3
+banyak = 1
+hasil = jumlah + banyak
+print(hasil)
+print(3 + 1)
+jumlah = 3
+banyak = 1
+hasil = jumlah + banyak 
+print(hasil)
+
